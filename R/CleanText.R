a3332c35245abb35dbbf604253066c237e0cdd24
zahrakhoshmanesh <zkh@iastate.edu>
2019-03-29 22:52:06 -0500

    update man

---
+++ R/CleanText.R
@@ -0,0 +1,6 @@
+#' Clean text and buil term matrix for bag of words model.
+#'
+#' @param x A tsv file having two columns, review as text, label as binary.
+#' @export
+#' @return The term matrix converted to dataframe.
+CleanText <- function(){}
