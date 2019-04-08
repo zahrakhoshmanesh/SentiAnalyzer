75226bd239a28c3bc33ebefa1a3681c57ed8de79
zahrakhoshmanesh <zkh@iastate.edu>
2019-04-07 23:20:09 -0500

    implement and add bigram:done

--- R/CleanText.R
+++ R/CleanText.R
@@ -1,8 +1,8 @@
 
-#' Clean text and buil term matrix for bag of words model or TF DFI.
+#' Clean text and buil term matrix for bag of words,TF DFI and bi-gram.
 #'
 #' @param source_dataset A tsv file having two columns, review as text, label as binary.
-#' @param dtm-method 1 for bag of word, 2 for TF DFI.
+#' @param dtm-method 1 for bag of word, 2 for TF DFI, 3 for bigram.
 #' @param reductionrate how many percent of term matrix you want to keep,usually 0.999.
 #' @return dataframe "dataset" : The term matrix converted to dataframe plus target label.
 #' @author Zahra Khoshmanesh
@@ -16,11 +16,12 @@
 
 CleanText <- function(source_dataset,dtm_method,reductionrate){
 
-  assert_that(not_empty(source_dataset), noNA(source_dataset),not_empty(dtm_method),not_empty(reductionrate))
+  #assertthat(not_empty(source_dataset), noNA(source_dataset),not_empty(dtm_method),not_empty(reductionrate))
 
   library(tm)
   library(rJava)
   library(RWeka)
+  library(matlib)
   source_datasets=read.delim(source_dataset,quote='',stringsAsFactors = FALSE)
   corpus=VCorpus(VectorSource(source_datasets$Review))
   #convert all review to lower case
@@ -35,13 +36,15 @@ CleanText <- function(source_dataset,dtm_method,reductionrate){
   corpus=tm_map(corpus,stemDocument)
   # remove extra space that created in cleaning stage when for example number remove
   corpus=tm_map(corpus,stripWhitespace)
+
   #creating document term matrix of words in reviews
-  # Trigrams
-  bigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 2))
+
+  # bigram
+  BigramTokenizer <-  function(x)  unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)
   dtm <-switch(dtm_method,
                '1' = DocumentTermMatrix(corpus),
                '2' = DocumentTermMatrix(corpus,control = list(weighting = function(x) weightTfIdf(x, normalize = FALSE))),
-               '3' = TermDocumentMatrix(corpus, control = list(tokenize = bigramTokenizer))
+               '3' = t(TermDocumentMatrix(corpus, control = list(tokenize = BigramTokenizer)))
                 )
 
   # reduce dimention of sparse matrix
@@ -51,8 +54,7 @@ CleanText <- function(source_dataset,dtm_method,reductionrate){
   dataset = as.data.frame(as.matrix(dtm))
   # encode the target feature as factor
   dataset$target = factor(source_datasets[[-1]],level=c(0,1))
-  assert_that(not_empty(dataset), noNA(dataset),is.data.frame(dataset))
-
+  #assertthat(not_empty(dataset), noNA(dataset),is.data.frame(dataset))
   return(dataset)
 }
 #df<-CleanText('./inst/Restaurant_Reviews.tsv',dtm_method=3,reductionrate=0.999)
