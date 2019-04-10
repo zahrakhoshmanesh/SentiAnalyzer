438c9d7af97a847674912e62279882e63f76368b
zahrakhoshmanesh <zkh@iastate.edu>
2019-04-09 23:31:33 -0500

    finish VisualizeText function

--- R/VisualizeData.R
+++ R/VisualizeData.R
@@ -12,53 +12,57 @@
 
 VisualizeData<-function(dataset){
 
-  library(tidyverse)
-  library(tm)
-  library(qdap)
+  library(tidytext)
+  library(dplyr)
+  library(ggplot2)
+  library(wordcloud)
+  library(reshape2)
 
-  #source_datasets=read.delim(dataset,quote='',stringsAsFactors = FALSE)
-  source_datasets=read.delim('./inst/Restaurant_Reviews.tsv',quote='',stringsAsFactors = FALSE)
+  source_datasets=read.delim(dataset,quote='',stringsAsFactors = FALSE)
+  #source_datasets=read.delim('./inst/Restaurant_Reviews.tsv',quote='',stringsAsFactors = FALSE)
 
-  #check the number of two class label
-  check_class <-table(source_datasets[[2]])
-  class_label<-names(source_datasets)[2]
-  class_text<-names(source_datasets)[1]
-  if (check_class[1]!=check_class[2]){
 
-    print("dataset is imbalance, balance it with calling BalanceData ")
+  text_df <- tibble(text = source_datasets[[1]])
 
-  }  else {
-    print("dataset is balanced dataset and no need to balance it")
-  }
+  tidy_text <- text_df %>%
+    unnest_tokens(word, text)
 
-  corpus=VCorpus(VectorSource(source_datasets[[1]]))
-  #convert all review to lower case
-  corpus= tm_map(corpus,content_transformer(tolower))
-  # remove numbers from reviews
-  corpus=tm_map(corpus,removeNumbers)
-  # remove punctuations from reviews
-  corpus=tm_map(corpus,removePunctuation)
-  # remove Stop words from reviews
-  corpus=tm_map(corpus,removeWords,stopwords())
-  # Stemming
-  corpus=tm_map(corpus,stemDocument)
-  # remove extra space that created in cleaning stage when for example number remove
-  corpus=tm_map(corpus,stripWhitespace)
-  myTDM <- TermDocumentMatrix(corpus)
-  findFreqTerms(myTDM,lowfreq = 20, highfreq = Inf)
+  data(stop_words)
+  tidy_text <- tidy_text %>%
+    anti_join(stop_words)
+
+  tidy_text  %>%
+    count(word, sort = TRUE)
+
+
+ wordfreqplot= tidy_text %>%
+    count(word, sort = TRUE) %>%
+    filter(n > 10) %>%
+    mutate(word = reorder(word, n)) %>%
+    ggplot(aes(word, n)) +
+    geom_col() +
+    xlab(NULL) +
+    coord_flip()
+ wordfreqplot
 
 
 
-  a <- tolower(source_datasets[[1]])
-  a <- removePunctuation(a)
-  a <- removeNumbers(a)
-  a <-rm_stopwords(a, tm::stopwords("english"))
+ wordcloadplot= tidy_text %>%
+    anti_join(stop_words) %>%
+    count(word) %>%
+    with(wordcloud(word, n, max.words = 100))
+
+ wordcloadplot
 
 
-  freq_term<-freq_terms(a)
-  ## S3 method for class 'freq_terms'
-  plot(freq_term, plot = TRUE)
 
+ reshapplot= tidy_text %>%
+    inner_join(get_sentiments("bing")) %>%
+    count(word, sentiment, sort = TRUE) %>%
+    acast(word ~ sentiment, value.var = "n", fill = 0) %>%
+    comparison.cloud(colors = c("gray20", "gray80"),
+                     max.words = 100)
+ reshapplot
 
 }
 
