4d2240e54ae45b087fa25d8bdd399b868289418d
atousaz <atousa.zarindast@gmail.com>
2019-04-21 11:47:36 -0500

    update shiny

---
+++ shiny/Atousa/Shiny3.R
@@ -0,0 +1,171 @@
+library(shiny)
+library(caret)
+
+library(tidyr)
+library(tidyverse)
+library(RWeka)
+library(eply)
+library(ggplot2)
+#install.packages("shinythemes") to run code
+x<-read.csv("../Data/x.csv")
+ui = tagList(
+    shinythemes::themeSelector(),
+    navbarPage(
+      # theme = "cerulean",  # <--- To set and use a specific theme, uncomment this
+      "ML training algorithms",
+      #sidebarLayout(
+               sidebarPanel(
+
+
+                 radioButtons(
+                   "whichM",
+                   "Training Alg",
+                   choices = c("knn", "gbm"),
+                   selected = "knn"),
+
+
+                 conditionalPanel(
+                   condition = "input.whichM=='knn'",
+
+                   numericInput("knn_min", "min number of neigbors: ", value = 1, min=1, max=50),
+                   numericInput("knn_max", "max number of neigbors: ", value = 3, min=1, max=50)),
+
+
+                 conditionalPanel(
+                   condition = "input.whichM=='gbm'",
+
+                   numericInput("n_l", "min number of trees:",
+                                value = 1, min=5, max=500),
+                   numericInput("n_m", "max number of trees:",
+                                value = 3, min=5, max=500)),
+
+
+
+                mainPanel(
+                 tabsetPanel(
+                   tabPanel("Visual",plotOutput("train")),
+                   tabPanel("summary",verbatimTextOutput("summary"))
+                            )
+                          )
+               )))
+
+
+server <- function(input, output) {
+  #datasetInput <- reactive({
+  #input$whichM
+  ctrl_cv10 = trainControl(
+    method = "cv",
+    number = 5,
+    savePred = T,
+    classProb = T
+  )
+  output$train <- renderPlot({
+    # train control
+    ctrl_cv10 = trainControl(
+      method = "cv",
+      number = 5,
+      savePred = T,
+      classProb = T
+    )
+    if (input$whichM=="knn"){
+
+    res_name <- colnames(x)[ncol(x)]
+    formula <- as.formula(paste(res_name, ' ~ .'))
+
+
+    treeGrid_knn = expand.grid(k = c(input$knn_min:input$knn_max))
+
+
+
+    ###### kNN with Cross Validation ############
+    model_knn_10 = train(
+      formula,
+      data = x,
+      method = "knn",
+      trControl = ctrl_cv10,
+      tuneGrid = treeGrid_knn
+    )
+
+    plot(model_knn_10)}
+
+
+
+    else if (input$whichM=="gbm"){
+
+      gbmGrid <-  expand.grid(
+        interaction.depth = c(1, 5, 9),
+        n.trees = c(input$n_l:input$n_m) * 50,
+        shrinkage = 0.1,
+        n.minobsinnode = 20
+      )
+
+    gbmFit2 <- train(
+      formula,
+      data = x,
+      method = "gbm",
+
+      verbose = FALSE,
+
+      tuneGrid = gbmGrid,
+      trControl = ctrl_cv10
+    )
+
+    plot(gbmFit2)}
+  })
+
+  # Generate a summary of the training ----
+  output$summary <- renderPrint({
+
+
+    if (input$whichM=="knn"){
+
+      res_name <- colnames(x)[ncol(x)]
+      formula <- as.formula(paste(res_name, ' ~ .'))
+
+
+      treeGrid_knn = expand.grid(k = c(input$knn_min:input$knn_max))
+
+
+
+      ###### kNN with Cross Validation ############
+      model_knn_10 = train(
+        formula,
+        data = x,
+        method = "knn",
+        trControl = ctrl_cv10,
+        tuneGrid = treeGrid_knn)
+
+      print(model_knn_10)
+
+
+    }
+    else if (input$whichM=="gbm"){
+      gbmGrid <-  expand.grid(
+        interaction.depth = c(1, 5, 9),
+        n.trees = c(input$n_l:input$n_m) * 50,
+        shrinkage = 0.1,
+        n.minobsinnode = 20
+      )
+
+      gbmFit2 <- train(
+        formula,
+        data = x,
+        method = "gbm",
+
+        verbose = FALSE,
+
+        tuneGrid = gbmGrid,
+        trControl = ctrl_cv10
+      )
+      print(gbmFit2)
+
+      }
+
+
+ # }#for reactive
+  })
+}
+# Bind ui and server together
+shinyApp(ui, server)
+
+
