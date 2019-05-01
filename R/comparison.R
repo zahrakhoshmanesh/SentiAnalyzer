8a3aabbf31fd326f4ec68125116c2a96576b253a
atousaz <atousa.zarindast@gmail.com>
2019-05-01 18:51:05 -0500

    updating  functions and shiny

--- R/comparison.R
+++ R/comparison.R
@@ -1,74 +1,41 @@
-#' compare the performance of the 4 algorithms 
+#' visualize performance of the algorithm
 #'
-#' @param x input is a dataframe for document-term matrix in which columns are the terms and row are binary variable 1 if that term exist in that data instance
-#' @return visualization of trained algorithms (for classifiying textual data), comparision and select the best!
+#' @param x input is a dataframe for bag of word file in which columns are the terms and row are binary variable 1 if that term exist in that data instance
+#' @return visualization of trained algrithms (for classifiying textual data), comparision and select the best!
 #' @author Atousa Zarindast
-#' @export 
-#' @import tidyverse
-#' @import tidyr
-#' @import assertthat
-#' @import testthat
-#' @import caret
-#' @import tidyr
+#' @export
 #' @examples
 #' library(SentiAnalyzer)
 #' csv_data <- read.csv(system.file(package = "SentiAnalyzer", "extdata/testing.csv"))
-#' my_training_data <- BuildTraining(csv_data)
-
+#' comparison <- comparison(csv_data)
 comparison <- function(x){
+  
+  if (ncol(x) > 1) {
+    df<-BuildPrediction(x)
+  } else {
+    # Assume x comes from BuildPrediction function
+    # Add more checks here to make sure the list is properly formatted!!
+    df <- x
+  }
+  
+  accuracy_f=function(x){(x$table[1]+x$table[2,2])/(x$table[1]+x$table[1,2]+x$table[2,1]+x$table[2,2])}
+  precision_f=function(x){(x$table[1])/(x$table[1]+x$table[1,2])}
+  recall_f=function(x){(x$table[1])/(x$table[1]+x$table[2,1])}
+  f1score= function(x){2 * ((x$table[1])/(x$table[1]+x$table[1,2]))*((x$table[1])/(x$table[1]+x$table[2,1]))   / (((x$table[1])/(x$table[1]+x$table[1,2])) +  (x$table[1])/(x$table[1]+x$table[2,1]))}
 
-  df<-BuildPrediction(x)
-  #gather the numbers for the confusion matrix: True Positive, True Negative, False positive, False Negative
-  #embed in formula
+
   
-  accuracy_f= function(x){
-    (x$table[1]+x$table[2,2])/(x$table[1]+x$table[1,2]+x$table[2,1]+x$table[2,2])}
-  precision_f= function(x){
-    (x$table[1])/(x$table[1]+x$table[1,2])}
-  recall_f= function(x){
-    (x$table[1])/(x$table[1]+x$table[2,1])}
-  f1score= function(x){
-    2*((x$table[1])/(x$table[1]+x$table[1,2]))*((x$table[1])/(x$table[1]+x$table[2,1])) / 
-      (((x$table[1])/(x$table[1]+x$table[1,2])) +  (x$table[1])/(x$table[1]+x$table[2,1]))}
-  
-  #pass the functions accuracy, precision, recall, f1score for each of the 4 algorithms
-  df<-df%>%mutate(accuracy=purrr::map(.x=conf,.f=accuracy_f),
+  df<-df%>%dplyr::mutate(accuracy=purrr::map(.x=conf,.f=accuracy_f),
                   precision=purrr::map(.x=conf,.f=precision_f),
                   recall=purrr::map(.x=conf,.f=recall_f),
                   f1score=purrr::map(.x=conf,.f=f1score))
+
+  
+  funm<-function(x){
+    as.data.frame(unlist(x))
+  }
+  df<-df[-1]%>%purrr::map(.f=funm)%>%as.data.frame()%>%dplyr::mutate(method=c("gbm","KNN","NB","RandomForest","SVM"))
+  names(df)<-(c("Accuracy","Precision","Recall","F1","Method"))
+
   return(df)
-}
-
-heatmapCmx <- function(listCmx) { #listCmx is list returned by BuildPrediction function
-  
-  Cmx_4param <- Cmx[2:5]
-  Cmx_4param <- as.data.frame(Cmx_4param) 
-  colnames(Cmx_4param) <- c("accuracy","precision","recall","f1-score")
-  rownames(Cmx_4param) <- c("Decision tree","Naive Bayes","k-Nearest Neighbors", "Gradient-Boosting")
-  
-  plotly::plot_ly(z=data.matrix(Cmx_4param),
-                  x=colnames(Cmx_4param),
-                  y=rownames(Cmx_4param), 
-                  type="heatmap")
-  
-  plotly::plot_ly(
-    type = 'table',
-    header = list(
-      values = c('<b>Confusion Matrix</b>', colnames(Cmx_4param)),
-      line = list(color = '#506784'),
-      fill = list(color = '#119DFF'),
-      align = c('left','center'),
-      font = list(color = 'white', size = 12)
-    ),
-    cells = list(
-      values = rbind(rownames(Cmx_4param), 
-                     data.matrix(Cmx_4param)),
-      line = list(color = '#506784'),
-      fill = list(color = c('#25FEFD', 'white')),
-      align = c('left', 'left'),
-      font = list(color = c('#black'), size = 12)
-    ))
-  
-  
-
 }
